5d9fb328611e751fc3071c4bc177feed911689d5
yunus.gun <yunus.gun@trendyol.com>
2023-07-12 20:51:29 +0300

    add pyro

--- src/main/java/com/samplejavaapp/app/DemoApplication.java
+++ src/main/java/com/samplejavaapp/app/DemoApplication.java
@@ -2,6 +2,7 @@ package com.samplejavaapp.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.PostConstruct;
 import io.pyroscope.javaagent.PyroscopeAgent;
 import io.pyroscope.javaagent.config.Config;
 import io.pyroscope.javaagent.EventType;
@@ -10,20 +11,19 @@ import io.pyroscope.http.Format;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-}
-@PostConstruct
-public void init() {
-
-    PyroscopeAgent.start(
-    new Config.Builder()
-        .setApplicationName("yunus-sample-app-java")
-        .setProfilingEvent(EventType.ITIMER)
-        .setFormat(Format.JFR)
-        .setServerAddress("http://35.224.186.90:30334")
-        .build()
-    );
+    @PostConstruct
+    public void init() {
+        PyroscopeAgent.start(
+            new Config.Builder()
+                .setApplicationName("yunus-sample-app-java")
+                .setProfilingEvent(EventType.ITIMER)
+                .setFormat(Format.JFR)
+                .setServerAddress("http://35.224.186.90:30334")
+                .build()
+        );
+    }
 }
