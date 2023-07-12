eca7df13699cb9996c484dd175ba3dfca149ede2
yunus.gun <yunus.gun@trendyol.com>
2023-07-12 20:39:03 +0300

    change version

--- src/main/java/com/samplejavaapp/app/DemoApplication.java
+++ src/main/java/com/samplejavaapp/app/DemoApplication.java
@@ -23,11 +23,7 @@ public void init() {
         .setApplicationName("yunus-sample-app-java")
         .setProfilingEvent(EventType.ITIMER)
         .setFormat(Format.JFR)
-        .setServerAddress("http://35.224.186.90:30334/?query=pyroscope.server.alloc_objects%7B%7D")
-        // Optionally, if authentication is enabled, specify the API key.
-        // .setAuthToken(System.getenv("PYROSCOPE_AUTH_TOKEN"))
-        // Optionally, if you'd like to sets allocation threshold to register events, in bytes. '0' registers all events
-        // .setProfilingAlloc("0")
+        .setServerAddress("http://35.224.186.90:30334")
         .build()
     );
 }
